3d19d9b027cf637424a84a806695d0564652861b
Chu A. <chu.amk22@gmail.com>
2025-10-01 12:55:53 +0100

    feat: show error message function

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const addBtn = document.getElementById('add-btn')
+const todoInput = document.getElementById('todo-input')
+const todoList = document.getElementById('todo-list')
+const pagination = document.getElementById('pagination')
+
+console.log(addBtn)
+
+const todos = []
+
+const showErrorMessage = (message) => {
+  const errorMessage = document.querySelector('.error-message')
+  errorMessage.textContent = message
+  errorMessage.style.display = 'block'
+  setTimeout(() => {
+    errorMessage.style.display = 'none'
+  }, 3000)
+}
